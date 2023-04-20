e7f4b9568b98730f4d43601fab81ca6940e626b4
alikaganbertizlioglu <alikaganbertizlioglu@gmail.com>
2023-04-20 21:36:56 +0300

    açıklama yazılacak

---
+++ MySQL/Database Sample Datas.sql
@@ -0,0 +1,157 @@
+-- I used random name,number generators for people table
+USE `JailManagementSystem` ;
+
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Kial', 'Larsen', '1965-12-23', '65551 Packers Alley', 'Female', '4268167592');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Dalila', 'Hurry', '1993-06-10', '195 Riverside Terrace', 'Female', '8773978702');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Ellene', 'Bausmann', '2008-06-10', '9379 Banding Circle', 'Female', '1257504049');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Eirena', 'Mussettini', '1993-08-21', '6014 Tennyson Street', 'Female', '3575491151');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Tiphanie', 'Tilby', '1953-05-15', '51 Milwaukee Street', 'Female', '9016288113');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Kara-lynn', 'Yendle', '1958-02-08', '97298 Gulseth Pass', 'Agender', '2918250625');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Demetria', 'Swainsbury', '2008-06-14', '9 Reinke Avenue', 'Female', '2217042211');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Devan', 'Millichip', '1965-07-09', '616 Clyde Gallagher Center', 'Female', '7527374439');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Rennie', 'Krolle', '1954-11-02', '71 Prairie Rose Crossing', 'Female', '4901061926');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Brigitta', 'Anstee', '2010-12-11', '63654 Buena Vista Street', 'Non-binary', '7397141742');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Eberhard', 'Dillingston', '1968-12-19', '72 Holy Cross Way', 'Male', '6643683699');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Chester', 'Birchenhead', '2004-11-15', '53449 Delladonna Lane', 'Male', '8268196931');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Becky', 'Brosio', '1986-01-15', '11 Warner Street', 'Female', '6933692481');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Sherill', 'Jorge', '1978-01-07', '623 Pennsylvania Lane', 'Female', '6193488753');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Archibald', 'Grist', '2013-05-12', '9 Jenifer Point', 'Male', '6518158625');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Cornie', 'Clemintoni', '1990-06-15', '33 Fuller Junction', 'Female', '5046539560');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Cullie', 'Annion', '2004-01-27', '20 Macpherson Park', 'Male', '6699437738');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Ekaterina', 'Andrioni', '1987-07-17', '16804 Morningstar Parkway', 'Female', '4425059911');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Remington', 'Elgee', '1998-08-03', '3318 Sunbrook Point', 'Male', '8892771955');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Kelley', 'Taylerson', '1981-09-14', '2 Miller Crossing', 'Female', '2743896763');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Morgen', 'Hillhouse', '1969-01-25', '456 Thackeray Plaza', 'Male', '1712797184');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Lorita', 'Jammet', '1984-03-27', '9597 Fallview Point', 'Female', '9229312243');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Kevin', 'Dillimore', '1980-12-24', '29 American Ash Plaza', 'Male', '7123585523');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Neel', 'Ramel', '2017-06-03', '7 Scoville Center', 'Male', '5784918173');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Idaline', 'Kollach', '2000-09-11', '8166 Sunfield Parkway', 'Female', '2308323990');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Ced', 'Casaccia', '1957-01-31', '4481 Bultman Court', 'Male', '7487804783');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Bud', 'Masse', '1966-07-08', '5438 Melrose Drive', 'Male', '5188182898');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Florenza', 'Botton', '1993-04-16', '7364 Mccormick Alley', 'Female', '7095571338');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Etti', 'O''Halligan', '1986-10-03', '88 Roth Avenue', 'Female', '2793622265');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Konstantine', 'Sandwick', '1966-05-02', '34787 Spohn Place', 'Male', '7249863177');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Billie', 'Penright', '2017-12-28', '9 7th Trail', 'Male', '8381496344');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Iver', 'Woolaghan', '1966-10-01', '459 Rutledge Court', 'Male', '2584329532');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Haley', 'Deeble', '1995-06-06', '587 Myrtle Terrace', 'Male', '5323916498');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Hazel', 'Lies', '2021-10-23', '5223 Springview Place', 'Male', '9919651287');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Moritz', 'Dries', '1983-12-01', '84 Rieder Junction', 'Male', '4709506555');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Breanne', 'Ryman', '1964-05-25', '75627 Laurel Drive', 'Female', '4052677968');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Lacee', 'Gottschalk', '2011-12-13', '8 5th Terrace', 'Female', '8634894144');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Mariette', 'Dewane', '2015-11-30', '6 Logan Road', 'Female', '7144611830');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Elka', 'Dunne', '1992-08-07', '97 Superior Road', 'Female', '7923214618');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Erwin', 'Arnett', '1983-07-22', '6840 Debra Street', 'Male', '7895944503');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Velma', 'Norval', '1962-09-04', '05 Prairie Rose Crossing', 'Female', '8211795350');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Renee', 'Valentino', '1961-04-04', '43420 Morning Way', 'Female', '1858091962');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Candis', 'Physic', '1954-07-05', '96798 American Place', 'Female', '5402319085');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Waiter', 'Trowell', '1992-06-05', '65997 Bonner Lane', 'Male', '9224955995');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Zeke', 'Dinsell', '1984-02-02', '0 Sheridan Trail', 'Male', '6309243207');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Jakie', 'Wrinch', '1980-02-13', '4541 Commercial Parkway', 'Male', '4705878967');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Giuseppe', 'Scardifeild', '1981-04-22', '42 David Avenue', 'Male', '8825142629');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Rennie', 'Woolforde', '2001-12-12', '5532 Packers Park', 'Female', '4976247086');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Benjie', 'Darwin', '2000-06-02', '96 Ramsey Alley', 'Male', '8727712848');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Janine', 'Twinbrow', '1956-09-19', '35 Emmet Parkway', 'Genderqueer', '5316340180');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Trix', 'McGrirl', '2009-08-17', '21410 Corry Plaza', 'Female', '9297041334');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Beilul', 'Creek', '1975-06-21', '56 Fisk Circle', 'Female', '3814995243');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Guinevere', 'Misselbrook', '1969-07-08', '3 Bobwhite Road', 'Female', '4541578072');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Hubey', 'Aujouanet', '1995-07-15', '57427 Pierstorff Hill', 'Male', '6697182952');
+insert into people (Name, Surname, Birthdate, Adress, gender, phoneNumber) values ('Casie', 'Obington', '2016-11-01', '0732 Heath Junction', 'Female', '4072027933');
+select * from people;
+
+insert into visitors(People_peopleID,numberofvisit) values (2,5);
+insert into visitors(People_peopleID,numberofvisit) values (33,25);
+insert into visitors(People_peopleID,numberofvisit) values (41,15);
+insert into visitors(People_peopleID,numberofvisit) values (22,13);
+insert into visitors(People_peopleID,numberofvisit) values (27,72);
+insert into visitors(People_peopleID,numberofvisit) values (31,12);
+insert into visitors(People_peopleID,numberofvisit) values (53,22);
+select * from visitors;
+
+insert into visits(Visitors_People_peopleID,visitDate,ReasonOfVisit) values (27,'2011-11-11','For seeing prisoner 30');
+insert into visits(Visitors_People_peopleID,visitDate,ReasonOfVisit) values (33,'2013-05-07','Prisoner physology researcher');
+insert into visits(Visitors_People_peopleID,visitDate,ReasonOfVisit) values (27,'2022-09-10','For seeing prisoner 30 which is his dad');
+select * from visits;
+
+insert into staffpositions(salary,jobtitle,dutyexplanation) values (95000, 'IT Support','Maintaining servers and computers');
+insert into staffpositions(salary,jobtitle,dutyexplanation) values (78500, 'Accountant','Accountant helps businesses make critical financial decisions by collecting, tracking, and correcting the companys finances.');
+insert into staffpositions(salary,jobtitle,dutyexplanation) values (57500, 'Call center specialist','Answering phone calls');
+insert into staffpositions(salary,jobtitle,dutyexplanation) values (150000, 'Prison Manager','assigns tasks to the personnel and checks whether they are performing their duties. It is necessary to check whether the laws, regulations, regulations and rules are followed. Coordinates and controls account affairs. Collects information of detainees and communicates with them');
+select * from staffpositions;
+
+-- SORUN VARR
+insert into staff(People_peopleID,dateStarted,dateQuit,WorkingLength,Staffpositions_positionID) values (51,'2000-01-01','2020-01-01',20,1);
+insert into staff(People_peopleID,dateStarted,dateQuit,WorkingLength,Staffpositions_positionID) values (49,'2002-05-05','2022-01-01',20,2);
+insert into staff(People_peopleID,dateStarted,dateQuit,WorkingLength,Staffpositions_positionID) values (47,'2012-07-07','2030-01-01',20,3);
+select * from staff;
+
+
+
+insert into Guards(People_peopleID,salary,height,weight) values (3,75000,187.3,88);
+insert into Guards(People_peopleID,salary,height,weight) values (5,75000,181.7,81);
+insert into Guards(People_peopleID,salary,height,weight) values (7,75000,175.4,70);
+insert into Guards(People_peopleID,salary,height,weight) values (9,75000,190,100);
+select * from guards;
+
+insert into cells(cellName,cellCapacity) values ('7Belalar',9);
+insert into cells(cellName,cellCapacity) values ('Gangsta Cell',8);
+insert into cells(cellName,cellCapacity) values ('Ghetto Cell',10);
+select * from cells;
+
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (8,180,78,'Black',' scar in neck',1);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (11,190,78,'Brown',' tatto on his left leg',1);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (16,185,78,'Black',' scar in his face',1);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (19,177,78,'Black',' scar in his leg',2);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (23,179,78,'Blonde',' scar in his ear',3);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (26,181,78,'Blonde','nose piercing',2);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (43,182,78,'Brown','one eye is missing',3);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (52,183,78,'Brown','ear piercing',2);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (32,184,78,'Bald','one finger is missing',1);
+insert into Prisoners(People_peopleID,Height,Weight,Haircolor,SpesificApperanceSpecifications,Cells_cellID) values (44,194,108,'Bald','one leg is missing',1);
+select * from prisoners;
+
+insert into OldPrisoners(Prisoners_People_peopleID,releasedate,reasonofrelease) values (8,'1999-05-07','End of jailtime');
+insert into OldPrisoners(Prisoners_People_peopleID,releasedate,reasonofrelease) values (11,'2021-05-07','Dead');
+insert into OldPrisoners(Prisoners_People_peopleID,releasedate,reasonofrelease) values (44,'2009-05-07','Escape');
+select * from oldprisoners;
+
+insert into PrisonerProblems(issueDescription,dangerLevel) values ('tried to escape',10);
+insert into PrisonerProblems(issueDescription,dangerLevel) values ('using bad words',1);
+insert into PrisonerProblems(issueDescription,dangerLevel) values ('resisting to guards',7);
+insert into PrisonerProblems(issueDescription,dangerLevel) values ('fighting with cellmate',5);
+insert into PrisonerProblems(issueDescription,dangerLevel) values ('fighting with cellmate',5);
+select * from prisonerproblems;
+
+insert into prisonerproblems_has_prisoners (prisonerproblems_problemID,Prisoners_People_peopleID) values (1,16);
+insert into prisonerproblems_has_prisoners (prisonerproblems_problemID,Prisoners_People_peopleID) values (2,19);
+insert into prisonerproblems_has_prisoners (prisonerproblems_problemID,Prisoners_People_peopleID) values (3,23);
+insert into prisonerproblems_has_prisoners (prisonerproblems_problemID,Prisoners_People_peopleID) values (4,26);
+insert into prisonerproblems_has_prisoners (prisonerproblems_problemID,Prisoners_People_peopleID) values (5,43);
+select * from prisonerproblems_has_prisoners;
+
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,Prisoners_People_peopleID) values ('Killed his wife',8,'1990-01-01',16);
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,Prisoners_People_peopleID) values ('Killed random dude',8,'1997-07-10',19);
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,Prisoners_People_peopleID) values ('Bank robbery',5,'2010-06-06',8);
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,punishmentEndDate,Prisoners_People_peopleID) values ('Stealing jewelry',4,'2010-04-06','2022-11-11',32);
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,punishmentEndDate,Prisoners_People_peopleID) values ('Resisting police officer',2,'2011-01-06','2019-04-10',44);
+insert into criminalcases (reasonOfCase,DangerLevel,punishmentStartDate,punishmentEndDate,Prisoners_People_peopleID) values ('Driving a car with alcohol',2,'2014-08-07','2018-08-10',11);
+
+
+select * from criminalcases;
+
+insert into prisonerpositions(JobTitle,DutyExplanation,hourlyFee) values ('Cleaner','Clean toilets',10.99);
+insert into prisonerpositions(JobTitle,DutyExplanation,hourlyFee) values ('Garbage collector','Collects garbages from the garden',10.99);
+insert into prisonerpositions(JobTitle,DutyExplanation,hourlyFee) values ('Cook Assistant','He peels potatoes',10.99);
+select * from prisonerpositions;
+
+insert into PrisonerStaff(Prisoners_People_peopleID,WorkingReason,StartWorkDate,PrisonerPositions_prisonerPositionID) values (43,'Wanted to make some money','2022-09-09',1);
+insert into PrisonerStaff(Prisoners_People_peopleID,WorkingReason,StartWorkDate,PrisonerPositions_prisonerPositionID) values (52,'Punishment of picking a fight','2021-06-09',2);
+insert into PrisonerStaff(Prisoners_People_peopleID,WorkingReason,StartWorkDate,PrisonerPositions_prisonerPositionID) values (23,'Wanted to make some money','2020-09-11',3);
+select * from prisonerstaff;
+
+
+
+
+
+
+
+
